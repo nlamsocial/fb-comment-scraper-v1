8f76cf86d1d6161bcafecd283d3115f6c40b069b
dinhbinh nguyen <dinhbinhnguyen0105@gmail.com>
2022-08-31 09:39:08 +0700

    posinset

--- src/content/index.js
+++ src/content/index.js
@@ -1,8 +1,36 @@
 chrome.runtime.onConnect.addListener((port) => {
     port.onMessage.addListener((req) => {
         if (req.action === 'scrapping') {
-            console.log(req.payload);
+            for (let i = 1; i < 5; i++) {
+                const posinsetElm =
+            }
         }
         port.postMessage({ status: 'successfully' });
     });
 });
+
+function waitForElm(selector) {
+    return new Promise((resolve) => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver((mutations) => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true,
+        });
+    });
+}
+
+function handleSuspendedFeed() {
+    const suspendedFeedElm = document.querySelector('.suspended-feed');
+    const progressbarElm = suspendedFeedElm.parentElement.querySelector('div[role="progressbar"]');
+    progressbarElm.scrollIntoView();
+}
